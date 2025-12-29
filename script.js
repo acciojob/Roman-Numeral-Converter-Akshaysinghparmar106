8ba83a68896eb02f9d7d12e91d6aba2d4b15dd3d
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2025-12-29 23:17:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,21 +10,30 @@ function convertToRoman(num) {
     };
 
   //your code here
-	 let result = "";
+	let num = 18; 
+// 700 + 90 + 8
+// 500 + 100 + 100 + 90 + 5 + 1 + 1 + 1
+//DCCXCVIII
 
-  for (let i = 0; i < romanMap.length; i++) {
-    while (num >= romanMap[i].value) {
-      result += romanMap[i].symbol;
-      num -= romanMap[i].value;
+let result = "";
+
+symbol.forEach((item) => {
+    // console.log(item);
+    // let val1 = item[0];
+    // let val2 = item[1];
+    // console.log(val1, val2);
+    const [key, val] = item; //destructuring of array
+    // console.log(key, val);
+
+    while(num >= val) {
+        num -= val;
+        result += key;
     }
-  }
+})
 
-  return result;
-
-}
-console.log(convertToRoman(14));   // XIV
-console.log(convertToRoman(798)); 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+console.log(result);
+	
+	// g the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
 
